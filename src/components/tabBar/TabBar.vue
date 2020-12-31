44c7085535693591472922aa5eaade2ba2b42a40
zhouyu0314 <181332969@qq.com>
2020-12-31 17:21:36 +0800

    alias

--- src/components/tabBar/TabBar.vue
+++ src/components/tabBar/TabBar.vue
@@ -1,26 +1,26 @@
 <template>
     <div id="tab-bar">
         <tab-bar-item path="/home">
-            <img slot="tab-bar-icon" src="../../assets/img/tabBar/home.svg">
-            <img slot="tab-bar-icon-active" src="../../assets/img/tabBar/home_active.svg">
+            <img slot="tab-bar-icon" src="~assets/img/tabBar/home.svg">
+            <img slot="tab-bar-icon-active" src="~assets/img/tabBar/home_active.svg">
             <div slot="tab-bar-text">首页</div>
         </tab-bar-item>
 
         <tab-bar-item path="/category">
-            <img slot="tab-bar-icon" src="../../assets/img/tabBar/category.svg">
-            <img slot="tab-bar-icon-active" src="../../assets/img/tabBar/category_active.svg">
+            <img slot="tab-bar-icon" src="~assets/img/tabBar/category.svg">
+            <img slot="tab-bar-icon-active" src="~assets/img/tabBar/category_active.svg">
             <div slot="tab-bar-text">分类</div>
         </tab-bar-item>
 
         <tab-bar-item path="/cart">
-            <img slot="tab-bar-icon" src="../../assets/img/tabBar/shopcart.svg">
-            <img slot="tab-bar-icon-active" src="../../assets/img/tabBar/shopcart_active.svg">
+            <img slot="tab-bar-icon" src="~assets/img/tabBar/shopcart.svg">
+            <img slot="tab-bar-icon-active" src="~assets/img/tabBar/shopcart_active.svg">
             <div slot="tab-bar-text">购物车</div>
         </tab-bar-item>
 
         <tab-bar-item path="/profile">
-            <img slot="tab-bar-icon" src="../../assets/img/tabBar/profile.svg">
-            <img slot="tab-bar-icon-active" src="../../assets/img/tabBar/profile_active.svg">
+            <img slot="tab-bar-icon" src="~assets/img/tabBar/profile.svg">
+            <img slot="tab-bar-icon-active" src="~assets/img/tabBar/profile_active.svg">
             <div slot="tab-bar-text">我的</div>
         </tab-bar-item>
     </div>
